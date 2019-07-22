600843d6e40c0acf3848b5bc8f720ce1bc3c2f45
stenwie <stenwie91@hotmail.it>
2019-07-22 12:36:00 +0200

    add animation to switch page by Id

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  RouteComponentProps,
+} from 'react-router-dom';
+import ChatRoomScreen from './components/ChatRoomScreen';
 import ChatsListScreen from './components/ChatsListScreen';
+import AnimatedSwitch from './components/AnimatedSwitch';
 
 const App: React.FC = () => {
+  
+  const redirectToChats = () => <Redirect to="/chats" />;
+
   return (
-    <div>
-      <ChatsListScreen />
-    </div>
+    <BrowserRouter>
+      <AnimatedSwitch>
+        <Route exact path="/chats" component={ChatsListScreen} />
+
+        <Route
+          exact
+          path="/chats/:chatId"
+          component={({ match }: RouteComponentProps<{ chatId: string }>) => (
+            <ChatRoomScreen chatId={match.params.chatId} />
+          )}
+        />
+      </AnimatedSwitch>
+      <Route exact path="/" render={redirectToChats} />
+    </BrowserRouter>
   );
 };
 
